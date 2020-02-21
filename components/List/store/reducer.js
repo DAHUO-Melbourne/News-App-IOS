d1215b56145e62402c73571ebc11f88a02baa34d
Da Huo <huodamelbourne@gmail.com>
2020-02-21 22:25:05 +1100

    refactor: change constants name in list reducer

--- components/List/store/reducer.js
+++ components/List/store/reducer.js
@@ -1,11 +1,11 @@
-import * as constants from './constants';
+import * as listConstants from './constants';
 import {fromJS, List} from 'immutable';
 const defaultState = fromJS({
   news: List(),
 });
 export default (state = defaultState, action) => {
   switch (action.type) {
-    case constants.INIT_LIST_DATA:
+    case listConstants.INIT_LIST_DATA:
       return state.set('news', action.data);
     default:
       return state;
